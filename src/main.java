c2cffb2d93d11d4beefbd4e2f3a1e476e4a237a1
Marcel Sikorski <marcelssk@gmail.com>
2021-09-30 00:31:20 +0200

    Config

--- src/main.java
+++ src/main.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
+
 public class main
 {
 
+    public static void main(String[] args)
+    {
+
+        double[] tab1 = {1, 2, 4.0, 7.0, 9.0};
+
+        System.out.println("tab1[0]: " + tab1[0]);
+        System.out.println("tab1[1]: " + tab1[1]);
+        System.out.println("tab1[2]: " + tab1[2]);
+        System.out.println("tab1[3]: " + tab1[3]);
+        System.out.println("tab1[4]: " + tab1[4]);
+
+    }
 }
