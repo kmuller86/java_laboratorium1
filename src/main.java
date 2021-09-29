5e7c74edafe4345e2977576a91415b558fb2bf39
Marcel Sikorski <marcelssk@gmail.com>
2021-09-29 22:27:01 +0200

    Config

--- src/main.java
+++ src/main.java
@@ -1,4 +1,53 @@
-public class main {
-int jedenchuj = 10;
-boolean chuj = false;
+import java.util.Scanner;
+
+public class main
+{
+
+    public static void main(String[] args)
+    {
+
+        int zmiennaInt = 7;
+        System.out.println("zmienna int = " + zmiennaInt);
+        /* ćw.2 */
+
+        /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+
+        /*
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
+
+
+        int zmienna = 10;
+        float zmienna1 = 10f;
+        boolean zmiennaBool = true;
+        char zmiennaChar = 'a';
+        String zmiennaPisana = "zmienna";
+
+
+        int liczbaA = 5;
+        int liczbaB = 6;
+
+        double liczbaX = 10.5;
+        double liczbaY = 5.5;
+
+        System.out.println(liczbaA + liczbaB);
+        System.out.println(liczbaA - liczbaB);
+        System.out.println(liczbaA * liczbaB);
+        System.out.println(liczbaA / liczbaB);
+        System.out.println(liczbaA % liczbaB);
+
+        System.out.println(liczbaX + liczbaY);
+        System.out.println(liczbaX - liczbaY);
+        System.out.println(liczbaX * liczbaY);
+        System.out.println(liczbaX / liczbaY);
+        System.out.println(liczbaX % liczbaY);
+
+
+
+
+    }
 }
+
