d7cb8039f857aeb1fa98126ca62f5f2b5d61bb50
Marcel Sikorski <marcelssk@gmail.com>
2021-09-29 22:24:58 +0200

    Config

--- src/main.java
+++ src/main.java
@@ -1,3 +1,16 @@
-public class main {
-int m = 1; erdh
+public class main
+{
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Witam serdecznie w cwiczeniu pierwszym");
+        System.out.println("Marcel" + " " + "Sikorski " + "Student WSB!");
+
+        System.out.printf("To %s implementacji %d opcji.\n", "prezentacja", 3);
+
+        System.out.print("To");
+        System.out.print(" krotki");
+        System.out.print(" tekst.");
+    }
 }
