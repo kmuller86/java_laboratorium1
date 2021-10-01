af8d757d1a9b4aeaf4992aa642acda01aea92ee6
Marcel Sikorski <marcelssk@gmail.com>
2021-10-01 21:12:20 +0200

    Config

--- src/main.java
+++ src/main.java
@@ -1,52 +1,45 @@
 import java.util.Scanner;
+import java.util.Locale;
 
-    public class main {
+public class main {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            int zmienna = 7;
-            /* pojedynczy if, kod z wewnątrz if-a jest wykonywany tylko gdy warunek jest prawdziwy,
-             * w innym wypadku kod jest pomijany przez program */
-            if (zmienna <= 10) {
-                System.out.println("1) zmienna jest mniejsza od 10");
-            }
+        /* ćw.5 */
 
-            /* konstrukcja else jest wykonywana tylko wtedy gdy wartość w if-ie jest nieprawdziwa,
-             * w innym wypadku kod z else-a jest pomijany */
-            if (zmienna > 10) {
-                System.out.println("2) zmienna jest wieksza od 10");
-            } else {
-                System.out.println("2) zmienna jest <= 10");
-            }
+        int nrIndeksu = 567088;
+        int wiek = 21;
+        Scanner scan = new Scanner(System.in);
+        scan.useLocale(Locale.US);
 
-            /* parametr trójargumentowy działa jak if else ale zapis w niektórych przypadkach
-             * jest bardziej estetyczny lub czytelny */
-            int wynik = (zmienna % 2 == 0) ? 0 : 1;
-            System.out.println("3) wynik: " + wynik);
+        /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
+         * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
+         * */
 
-            /* Konstrukcja else if */
-            if (zmienna < 5) {
-                System.out.println("4) zmienna < 5");
-            } else if (zmienna > 15) {
-                System.out.println("4) zmienna > 15");
-            } else if (zmienna == 10) {
-                System.out.println("4) zmienna == 10");
-            } else {
-                System.out.println("5) wykonał sie else, zmienna > 5 i < 15 i !10");
-            }
-
-            /* ćw.5 */
-
-            /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
-             * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
-             * */
-
-
-            /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
-
-
-            /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
-             * i stwórz konstrukcję else if z dowolnymi komunikatami */
-
+        if (wiek % 3 == 0) {
+            System.out.println("Wiek podzielny przez 3");
+        } else {
+            System.out.println("Wiek niepodzielny przez 3");
         }
+
+        /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
+
+        String podzielny = (nrIndeksu % 2 == 0) ? "Indeks parzysty" : "Indeks nieparzysty";
+        System.out.println("Wynik: " + podzielny);
+
+        /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
+         * i stwórz konstrukcję else if z dowolnymi komunikatami */
+
+        System.out.print("Wpisz liczbe zmiennoprzecinkowa: ");
+        double liczba = scan.nextDouble();
+
+        if (liczba > 5.0) {
+            System.out.println("Wpisana liczba jest wieksza od 5");
+        } else if (liczba < 5.0) {
+            System.out.println("Wpisana liczba jest wieksza od 5");
+        } else {
+            System.out.println("Wpisana liczba jest rowna 5");
+        }
+
     }
+}
